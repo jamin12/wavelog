31df39f23b7d75458cf192dc3ba4df2352e3f36d
kmeoung <kmeoung@gmail.com>
2021-07-11 22:21:36 +0900

    Design 변경 Main Widget 아이템 클릭 및 hover 시 이벤트 추가

--- README.md
+++ README.md
@@ -6,14 +6,21 @@ Kyoung-Min
 blog project initialized
 2021-05-12
 
+# Commit Record
+
+[![commitcombo](http://commitcombo.com/get?user={kmeoung}&theme={Sunset-mini})](https://github.com/devxb/CommitCombo)
+
 # 이전 작업에 대한 내용
+
 지금 뭘 하고 있는지 잘 모르겠다
 아무래도 Web에 대한 기초적인 강의를 정독하고
 그 후 provider에 대한 기초적인 강의 듣고
 다시 작업을 진행해야 맞는 것 같다
 https://www.youtube.com/watch?v=rhq5yImWdBw&list=PLIKnSA4GMR4O9uTZvKLq9UWx3I8biurBr&index=9
 여기 링크를 남긴다
+
 # VIEW 해야할 일
+
 <ol>
     <li>Splash Screen에서 사용자 클릭 시 선택되지 않은 사용자 사라지는 애니메이션 처리</li>
     <li>로그인 기능 생성
@@ -32,12 +39,14 @@ https://www.youtube.com/watch?v=rhq5yImWdBw&list=PLIKnSA4GMR4O9uTZvKLq9UWx3I8biu
 </ol>
 
 # VIEW 문제 사항
+
 <ol>
     <li>모바일 사이즈 화면 사이즈에서는 SVG에 색상이 들어가지 않음</li>
     <li>Web 및 Android의 기본 탑재된 뒤로가기 시 View 가 제대로 작동하지 않으며 작동한다 하더라도 다음번 부터는 뒤로가기 기능이 손실됨</li>
 </ol>
 
 # VIEW 수정 완료 사항
+
 <ol>
     <li>
         <del>WEB 전체화면 모드에서 SEA -> BEACH 로 전환 시 배경이 깨지는 문제 발생 (Release 모드에서만 발생) (애니메이션 최적화 후 사라진듯 함)</del>
@@ -55,4 +64,3 @@ https://www.youtube.com/watch?v=rhq5yImWdBw&list=PLIKnSA4GMR4O9uTZvKLq9UWx3I8biu
         <del>애니메이션 작동 시 최적화가 필요함 (해결방법 : AnimatedBuilder를 써서 필요한 부분만 rebuild 시킴)</del>
     </li>
 </ol>
-
--- view/lib/views/main_screen/widget/main_widget.dart
+++ view/lib/views/main_screen/widget/main_widget.dart
@@ -74,36 +74,41 @@ class MainWidget extends StatelessWidget {
           itemCount: _tempPost.length,
           itemBuilder: (context, index) => ClipRRect(
             borderRadius: BorderRadius.circular(15.0),
-            child: GestureDetector(
-              onTap: () {
-                itemClickListener(index);
-              },
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0,
-                    child: Icon(
-                      Icons.water_rounded,
-                      size: 300.0,
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {
+                  itemClickListener(index);
+                },
+                hoverColor: Colors.black,
+                splashColor: COLOR_BEACH,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      top: 0,
+                      bottom: 0,
+                      child: Icon(
+                        Icons.water_rounded,
+                        size: 300.0,
+                      ),
                     ),
-                  ),
-                  Container(
-                    color: COLOR_BLACK.withOpacity(0.9),
-                    child: Center(
-                      child: Text(
-                        _tempPost[index],
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30.0,
+                    Container(
+                      color: COLOR_BLACK.withOpacity(0.9),
+                      child: Center(
+                        child: Text(
+                          _tempPost[index],
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
